b540158e5fd8bfc73608a39f4f5abe50556176fb
Asadell <m.satrioasadell@gmail.com>
2025-12-26 13:55:52 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# routify_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/routify_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.routify_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
